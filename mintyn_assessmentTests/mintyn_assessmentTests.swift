354e96c5677575a0ca6a50f882da96deb0197d92
David Onoh <davidonoh@gmail.com>
2025-02-20 08:32:49 +0100

    Initial Commit

---
+++ mintyn_assessmentTests/mintyn_assessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  mintyn_assessmentTests.swift
+//  mintyn_assessmentTests
+//
+//  Created by DavidOnoh on 2/20/25.
+//
+
+import Testing
+@testable import mintyn_assessment
+
+struct mintyn_assessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
